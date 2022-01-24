b7da533c6ae344ab60a6b4f9c0f0bc7d042e636f
rmnk-nz <rua.manukau20@gmail.com>
2022-01-24 23:49:23 +1100

    updated typeDefs and reslovers to include mutations and auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,11 +11,37 @@ type Book {
  }
 
  type User {
+    _id: ID!
     username: String!
     email: String!
     savedBooks: [Book]
     bookCount: Int
  }
-`
+
+ type Auth {
+    token: ID!
+    user: User
+ }
+
+ type Query {
+    me: User
+ }
+
+ input bookInput {
+    bookId: String!
+    authors: [String!]
+    desription: String
+    title: String
+    image: String
+    link: String
+ }
+
+ type Mutation {
+      login(email: String!, password: String!): Auth
+      addUser(username: String!, email:String!, password: String!):Auth
+      saveBook(input: bookInput): User
+      removeBook(bookId: String!): User
+ }
+`;
 
 module.exports = typeDefs;
