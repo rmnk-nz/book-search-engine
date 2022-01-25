7b5d085d3d1534783e16bfc7a1939998b5ac0f51
rmnk-nz <rua.manukau20@gmail.com>
2022-01-26 09:57:56 +1100

    added mutations and queries to utils folder

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,45 +2,45 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs =gql`
 type Book {
-    authors: [String!]
-    description : String!
-    bookID : String!
-    image: String
-    link: String
-    title: String!
+   authors: [String!]
+   description : String!
+   bookID : String!
+   image: String
+   link: String
+   title: String!
  }
 
  type User {
-    _id: ID!
-    username: String!
-    email: String!
-    savedBooks: [Book]
-    bookCount: Int
+   _id: ID!
+   username: String!
+   email: String!
+   savedBooks: [Book]
+   bookCount: Int
  }
 
  type Auth {
-    token: ID!
-    user: User
+   token: ID!
+   user: User
  }
 
  type Query {
-    me: User
+   me: User
  }
 
  input bookInput {
-    bookId: String!
-    authors: [String!]
-    desription: String
-    title: String
-    image: String
-    link: String
+   bookId: String!
+   authors: [String!]
+   desription: String
+   title: String
+   image: String
+   link: String
  }
 
  type Mutation {
-      login(email: String!, password: String!): Auth
-      addUser(username: String!, email:String!, password: String!):Auth
-      saveBook(input: bookInput): User
-      removeBook(bookId: String!): User
+   login(email: String!, password: String!): Auth
+   addUser(username: String!, email:String!, password: String!): Auth
+   saveBook(input: bookInput): User
+   removeBook(bookId: String!): User
  }
 `;
 
