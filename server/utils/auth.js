f924aad3b3f2294e9b6256583068e306a0d4d803
rmnk-nz <rua.manukau20@gmail.com>
2022-01-26 23:56:29 +1100

    fixed login error

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ( { req }) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -23,13 +23,12 @@ module.exports = {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+    } catch(e) {
+      console.log(e)
     }
-
-    // send to next endpoint
-    next();
+    return req;
+    // // send to next endpoint
+    // next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
